fa2fdaacf8a703eb9e01b37aa4aec523f7691894
arooj fatima <arooj.fatima@gmail.com>
2015-06-15 21:54:42 +0100

    update
    updates

---
+++ www/print.js
@@ -0,0 +1,15 @@
+cordova.define("cordova/plugin/printPluginVar", function(require, exports, module) {
+    printPluginVar = function(){
+        var _file = 'base64';
+        var _context = null;
+    };
+    printPluginVar.initialize = function(obj) {
+        var _this = this;
+    };
+
+    printPluginVar.printIt = function(message, successCallback, errorCallback)
+	{  
+       cordova.exec(successCallback, errorCallback, "Print", "print", [message]);
+	};
+   	module.exports = printPluginVar;
+});
